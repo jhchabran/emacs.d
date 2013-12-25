d337dc9d194c025ea1176d86c40367e2eb5194e3
Jean-Hadrien Chabran <jh@chabran.fr>
2013-12-25 22:44:22 +0100

    Rework powerline, less clutter
    * config/init-eyecandy.el:

--- config/init-eyecandy.el
+++ config/init-eyecandy.el
@@ -13,23 +13,71 @@
 (require-package 'powerline)
 (require 'powerline)
 
+;; Basic powerline with Evil state and minus useless minor mode listing
+(defun jh/powerline-minimalist-theme ()
+  (interactive)
+  (setq-default mode-line-format
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+                                                          powerline-default-separator
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           powerline-default-separator
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" nil 'l)
+                                     ;;(powerline-buffer-size nil 'l)
+                                     (powerline-buffer-id nil 'l)
+                                     (powerline-raw " ")
+                                     (powerline-raw evil-mode-line-tag face1 'l)
+                                     (funcall separator-left mode-line face1)
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     ;;(powerline-minor-modes face1 'l)
+                                     ;;(powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     ;;(funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-raw "%4l" face1 'l)
+                                     (powerline-raw ":" face1 'l)
+                                     (powerline-raw "%3c" face1 'r)
+                                     ;;(funcall separator-right face1 mode-line)
+                                     (powerline-raw " ")
+                                     (powerline-raw "%6p" nil 'r)
+                                     (powerline-hud face2 face1))))
+                     (concat (powerline-render lhs)
+                             (powerline-fill face2 (powerline-width rhs))
+                                                          (powerline-render rhs)))))))
+
 ;; Use a less contrasted style
 (set-face-attribute 'mode-line nil
                     :foreground "cyan"
                     :background "black"
                     :inverse-video nil
                     :box nil)
+
+;; Kind of hide all inactive mode lines.
 (set-face-attribute 'mode-line-inactive nil
                     :inverse-video nil
+                    :foreground "black"
+                    :background "black"
                     :box nil)
-(powerline-center-evil-theme)
+(jh/powerline-minimalist-theme)
 
 ;; Get rid of the ugly vertical separator
 (set-face-background 'vertical-border "black")
 (set-face-foreground 'vertical-border "black")
 
 
-;; Well I don't remember what this one is
+;; Display symbols where it can, like "lambda" being replaced by λ in lisp modes
 (require-package 'pretty-mode)
 (setq pretty-default-groups '(:function))
 (require 'pretty-mode)
