fa4b93619f4a7f7553e2860453a8ae9a4391e1e4
Jean-Hadrien Chabran <jh@chabran.fr>
2013-12-25 23:21:12 +0100

    Remove Perspective since Projectile provides enough
    * config/init-projects.el:

--- config/init-projects.el
+++ config/init-projects.el
@@ -5,13 +5,6 @@
 (setq projectile-enable-caching t)
 (projectile-global-mode)
 
-(require-package 'perspective)
-(require 'perspective)
-(persp-mode)
-
-;; Interesting: create a named perspective and exec some code
-;; http://emacsrookie.com/2011/09/25/workspaces/
-
 (require-package 'company)
 (require 'company)
 
